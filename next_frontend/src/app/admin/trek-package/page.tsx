59017ed2f0b048bf95c61bb835968d485da8c93c
saroz96 <55934122+saroz96@users.noreply.github.com>
2026-04-09 20:44:26 +0545

    Change undefined to null for discountedPrice update

--- next_frontend/src/app/admin/trek-package/page.tsx
+++ next_frontend/src/app/admin/trek-package/page.tsx
@@ -2335,7 +2335,7 @@ const TrekPackageManager: React.FC = () => {
                                   "discountedPrice",
                                   e.target.value
                                     ? parseFloat(e.target.value)
-                                    : undefined,
+                                    : null,
                                 )
                               }
                               style={styles.input}
@@ -2521,7 +2521,7 @@ const TrekPackageManager: React.FC = () => {
                                   index,
                                   "maxTravelers",
                                   parseInt(e.target.value) ||
-                                    discount.minTravelers,
+                                  discount.minTravelers,
                                 )
                               }
                               style={styles.input}
